462c9d47aa5bba213c5b9dc438292c2ddcb9b152
Laush <laush_x4@hotmail.com>
2019-05-10 11:48:46 -0300

    'estructura'

---
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal.Controllers
+{
+    public class HomeController : Controller
+    {   
+        //Landing Page
+        public ActionResult Logout()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
