aabb87030a1c63e61af30a6a7a4173ad15df4e09
Laush <laush_x4@hotmail.com>
2019-05-28 14:46:52 -0300

    pantalla perfil usuario

---
+++ ProyectoFinal/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult IndexAdmin()
+        {
+            return View();
+        }
+        public ActionResult IndexViajero()
+        {
+            //List<Usuario> usus = new List<Usuario>();
+            //usus = srvUsuario.Listar();
+            // return View(usus);
+            return View();
+        }
+        public ActionResult IndexAgencia()
+        {
+            return View();
+        }
+    }
+}
